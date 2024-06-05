6ed0b98883d42cedb2bd88cdd4460123e83c1b58
DylanHenderson <dylan9h@gmail.com>
2024-06-06 01:39:36 +0200

    added config for hosting site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://dylanhenderson.github.io',
+	base: '/twisted-tree-website',
 	integrations: [
 		starlight({
 			title: 'Twisted Tree Dev',
