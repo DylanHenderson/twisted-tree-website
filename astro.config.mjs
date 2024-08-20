1f310768b118b013ed26b41667f02c1e6fdd6dbf
DylanHenderson <dylan9h@gmail.com>
2024-08-20 14:39:34 +0200

    Update Scan Picker Docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,8 +14,9 @@ export default defineConfig({
 				
 					items: [
 						//Each item here is one entry in the navigation menu.
-						
+						{ label: 'Getting started', link: '/scanpickerdocs/gettingstarted' },
 						{ label: 'Add barcodes to packing slips', link: '/scanpickerdocs/packingslips' },
+						{ label: 'Packing slips template', link: '/scanpickerdocs/packingslipstemplate' },
 						{ label: 'Changelog', link: '/scanpickerdocs/changelogs' },
 
 					],
