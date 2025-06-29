b46301aa969685648d1e14b75f94a6850061e45c
DylanHenderson <dylan9h@gmail.com>
2025-06-29 18:37:17 +0200

    Twist: Stock Take app documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,71 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://twistedtreedev.com',
-	integrations: [
-		starlight({
-			favicon: '/favicon.png',
-			title: 'Twisted Tree Dev',
-			sidebar: [
-				{
-					label: 'Scan Picker Documentation',
-				
-					items: [
-						//Each item here is one entry in the navigation menu.
-						{ label: 'Getting started', link: '/scanpickerdocs/gettingstarted' },
-						{ label: 'Add barcodes to packing slips', link: '/scanpickerdocs/packingslips' },
-						{ label: 'Packing slips template', link: '/scanpickerdocs/packingslipstemplate' },
-						{ label: 'Changelog', link: '/scanpickerdocs/changelogs' },
+  site: "https://twistedtreedev.com",
+  integrations: [
+    starlight({
+      favicon: "/favicon.png",
+      title: "Twisted Tree",
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Scan Picker Documentation",
 
-					],
-				},
-				// {
-				// 	label: 'Quick Stock Documentation',
-				
-				// 	items: [
-				// 		//Each item here is one entry in the navigation menu.
-						
-				// 		{ label: 'Getting started', link: '/quickscandocs/gettingstarted' },
-				// 		{ label: 'Settings adjustments', link: '/quickscandocs/settings' },
-				// 		{ label: 'Changelog', link: '/quickscandocs/changelogs' },
+          items: [
+            //Each item here is one entry in the navigation menu.
+            {
+              label: "Getting started",
+              link: "/scanpickerdocs/gettingstarted",
+            },
+            {
+              label: "Add barcodes to packing slips",
+              link: "/scanpickerdocs/packingslips",
+            },
+            {
+              label: "Packing slips template",
+              link: "/scanpickerdocs/packingslipstemplate",
+            },
+            { label: "Changelog", link: "/scanpickerdocs/changelogs" },
+          ],
+        },
+        {
+          label: "Twist: Stock Take Documentation",
 
-				// 	],
-				// },
-				{
-					label: 'Simple Calendar Documentation',
-				
-					items: [
-						//Each item here is one entry in the navigation menu.
-						
-						{ label: 'Getting started', link: '/simplecalendardocs/gettingstarted' },
-						{ label: 'Changelog', link: '/simplecalendardocs/changelogs' },
+          items: [
+            //Each item here is one entry in the navigation menu.
 
-					],
-				},							
-				{
-					label: 'Twisted Tree',
-					autogenerate: { directory: 'twistedTree' },
-				},
-			],
-		}),
-	],
+            { label: "Getting started", link: "/stocktakedocs/gettingstarted" },
+            { label: "Supported devices", link: "/stocktakedocs/devices" },
+
+            { label: "Stock take filters", link: "/stocktakedocs/filters" },
+            { label: "Stock take automations", link: "/stocktakedocs/automations" },
+            
+
+            { label: "Changelog", link: "/stocktakedocs/changelogs" },
+
+
+          ],
+        },
+        {
+          label: "Simple Calendar Documentation",
+
+          items: [
+            //Each item here is one entry in the navigation menu.
+
+            {
+              label: "Getting started",
+              link: "/simplecalendardocs/gettingstarted",
+            },
+            { label: "Changelog", link: "/simplecalendardocs/changelogs" },
+          ],
+        },
+        {
+          label: "Twisted Tree",
+          autogenerate: { directory: "twistedTree" },
+        },
+      ],
+    }),
+  ],
 });
